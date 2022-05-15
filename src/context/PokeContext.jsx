8aa53069f09f7173db11d982c424085ae6b651ca
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-15 19:04:05 -0300

    add function that brings data

--- src/context/PokeContext.jsx
+++ src/context/PokeContext.jsx
@@ -1,19 +1,27 @@
 import { createContext, useState, useEffect } from "react";
 export const PokeContext = createContext();
 const PokeProvider = ({ children }) => {
-  const [data, setData] = useState(0);
+  const [data, setData] = useState([]);
   const [list, setList] = useState([]);
-  useEffect(() => {
-    const fetchdata = async () => {
-      const url = ` https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20`;
-      const rta = await fetch(url);
-      const data = await rta.json();
-      setList(data.results);
-    };
-    fetchdata();
-  }, []);
+
+  const traerDatos = () => {
+    let pokedata = [];
+    for (let i = 0; i <= 151; i++) {
+      const fetchdata = async () => {
+        const url = ` https://pokeapi.co/api/v2/pokemon/${i}`;
+        const rta = await fetch(url);
+        const datos = await rta.json();
+        pokedata.push(datos);
+        pokedata.sort((a, b) => a - b);
+
+        setList(pokedata);
+      };
+      fetchdata();
+    }
+  };
+
   return (
-    <PokeContext.Provider value={{ data, setData, list, setList }}>
+    <PokeContext.Provider value={{ data, setData, list, setList, traerDatos }}>
       {children}
     </PokeContext.Provider>
   );
