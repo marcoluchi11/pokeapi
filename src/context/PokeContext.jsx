14ea9aaa17292cc37e77fd8dd5e498e25c8ab377
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-16 17:48:25 -0300

    add function for chose a pokemon for more info

--- src/context/PokeContext.jsx
+++ src/context/PokeContext.jsx
@@ -3,16 +3,17 @@ export const PokeContext = createContext();
 const PokeProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [list, setList] = useState([]);
-
+  const [more, setMore] = useState(false);
+  const [chosen, setChosen] = useState({});
   const traerDatos = () => {
     let pokedata = [];
-    for (let i = 0; i <= 151; i++) {
+    for (let i = 0; i <= 251; i++) {
       const fetchdata = async () => {
         const url = ` https://pokeapi.co/api/v2/pokemon/${i}`;
         const rta = await fetch(url);
         const datos = await rta.json();
         pokedata.push(datos);
-        pokedata.sort((a, b) => a - b);
+        // pokedata.sort((a, b) => a - b);
 
         setList(pokedata);
       };
@@ -21,7 +22,19 @@ const PokeProvider = ({ children }) => {
   };
 
   return (
-    <PokeContext.Provider value={{ data, setData, list, setList, traerDatos }}>
+    <PokeContext.Provider
+      value={{
+        chosen,
+        setChosen,
+        more,
+        setMore,
+        data,
+        setData,
+        list,
+        setList,
+        traerDatos,
+      }}
+    >
       {children}
     </PokeContext.Provider>
   );
