7c2236cbf9fa88b9d6ee5237612137385e2a790e
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-19 19:47:49 -0300

    add search and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import styled from "@emotion/styled";
 import PokemonList from "./components/PokemonList";
-
-const Titulo = styled.h1`
-  text-align: center;
+import Search from "./components/Search";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Imagen = styled.img`
   background-color: #b2b2b2;
+  align-self: center;
   padding: 1rem;
   margin: 0.5rem;
   font-size: 5rem;
+  width: 50%;
+  max-width: 100%;
+  height: 15rem;
 `;
 function App() {
   return (
-    <div>
-      <Titulo>Kanto & Johto Pokemon</Titulo>
+    <Container>
+      <Imagen
+        src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+        alt="pokeapi"
+      />
+      <Search />
       <PokemonList />
-    </div>
+    </Container>
   );
 }
 
