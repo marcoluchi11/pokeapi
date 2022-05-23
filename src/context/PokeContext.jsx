400e30be67747a46d3cff597172f1c21c30935d1
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-23 18:47:17 -0300

    delete useEffect

--- src/context/PokeContext.jsx
+++ src/context/PokeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 export const PokeContext = createContext();
 const PokeProvider = ({ children }) => {
   const [data, setData] = useState([]);
